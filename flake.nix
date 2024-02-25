907b271280ecf2a09a2abaa2f1a1bed5ee3cf2fa
nullishamy <git@amyerskine.me>
2024-02-25 16:47:35 +0000

    chore: optimise nix deps
    Co-authored-by: Hamothy <58985301+sgoudham@users.noreply.github.com>

--- flake.nix
+++ flake.nix
@@ -2,19 +2,16 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
     flake-utils.url = "github:numtide/flake-utils";
-    catppuccin-toolbox.url = "github:catppuccin/toolbox";
   };
 
   description = "Development shell flake";
-  outputs = { self, nixpkgs, flake-utils, catppuccin-toolbox }: flake-utils.lib.eachDefaultSystem (system: {
+  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system: {
     devShell = let 
       pkgs = nixpkgs.legacyPackages.${system};
     in 
       pkgs.mkShell {
         packages = with pkgs; [
-          just
           deno
-          catppuccin-toolbox.packages.${system}.whiskers
         ];
       };
   });
